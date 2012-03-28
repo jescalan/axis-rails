7a14e02022930d52a304780eeea50034cef16e00
Jeff Escalante <hello@jenius.me>
2012-03-28 13:52:37 -0400

    modification the generate so it's smoother if roots:install has already been run

--- lib/generators/roots/framework_generator.rb
+++ lib/generators/roots/framework_generator.rb
@@ -7,8 +7,10 @@ module Roots
 
       def generate
         if yes? "This will replace your application.css file - is this ok / have you backed it up? [y/n]", "\e[31m"
-          remove_file "app/assets/stylesheets/application.css"
-          copy_file "layouts/application.sass", "app/assets/stylesheets/application.sass"
+          if File.exist? "app/assets/stylesheets/application.css"
+            remove_file "app/assets/stylesheets/application.css"
+            copy_file "layouts/application.sass", "app/assets/stylesheets/application.sass"
+          end
           directory "sass", "app/assets/stylesheets"
         end 
         say "\nComplete! Make sure to visit application.sass to configure.\n\n", "\e[32m"
