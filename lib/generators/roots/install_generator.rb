cdebc43fe0175a95ad9ea4758056ef846fb3d155
Jeff Escalante <hello@jenius.me>
2012-03-28 12:40:09 -0400

    install task does bulk work, adds image and script dependencies, playing with asset url in stylesheets

--- lib/generators/roots/install_generator.rb
+++ lib/generators/roots/install_generator.rb
@@ -3,6 +3,21 @@ module Roots
   module Generators
     class InstallGenerator < Rails::Generators::Base
       desc "Loads sass framework, replaces layout, removes public/index.html"
+      source_root File.expand_path('../../templates', __FILE__)
+
+      def replace_layout
+        remove_file "app/views/layouts/application.html.erb"
+        copy_file "layouts/layout.html.haml", "app/views/layouts/application.html.haml"
+        copy_file "layouts/config.html.haml", "app/views/layouts/_config.html.haml"
+      end
+
+      def install_dependencies
+        copy_file "scripts/selectivizr.js", "vendor/assets/javascripts/selectivizr.js"
+        copy_file "scripts/pie.htc", "vendor/assets/javascripts/pie.htc" # asset-url('pie.htc', javascript)
+        remove_file "public/favicon.ico"
+        copy_file "favicon.ico", "public/favicon.ico"
+        directory "images", "public/roots"
+      end
 
       def install
         # logic here
--- lib/generators/roots/replace_layout_generator.rb
+++ lib/generators/roots/replace_layout_generator.rb
@@ -3,9 +3,12 @@ module Roots
   module Generators
     class ReplaceLayoutGenerator < Rails::Generators::Base
       desc "replaces the default layout with roots' awesome one"
+      source_root File.expand_path('../../templates', __FILE__)
 
       def replace_layout
-        # logic here
+        remove_file "app/views/layouts/application.html.erb"
+        copy_file "layouts/layout.html.haml", "app/views/layouts/application.html.haml"
+        copy_file "layouts/config.html.haml", "app/views/layouts/_config.html.haml"
       end
 
     end
